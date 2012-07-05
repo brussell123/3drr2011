79a8d2ffc8a27363dfe0195c1a070dfb78c93549
brussell123 <bcr@cs.washington.edu>
2012-07-05 14:37:55 -0700

    refreshed testBinaries

--- testBinaries.m
+++ testBinaries.m
@@ -2,4 +2,9 @@ addpath ./code;
 
 meshColoredFileName = './pompeii_large66_sample_0.1_poisson_depth_14_clean_colored.ply';
 
+% mexReadPly:
 [vertices,faces,colors] = mexReadPly(meshColoredFileName);
+
+% fastNN:
+x = single(rand(2,10));
+[aa,bb,vv,nn] = fastNN(x,x);

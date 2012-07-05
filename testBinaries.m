c9d0c915dd3fc3da1b841ea5ccc576c37d5289c6
brussell123 <bcr@cs.washington.edu>
2012-07-05 15:40:20 -0700

    added imageSize

--- testBinaries.m
+++ testBinaries.m
@@ -12,6 +12,7 @@ x = single(rand(2,10));
 % meshGenerateColored:
 BIN_PATH = './code/LIBS/GenerateLambertian/generate_colored';
 P = [108.792007 276.910461 -723.832397 -171.125000; 405.367004 654.604614 216.727722 -51.611908; 0.972418 -0.089354 -0.215453 -0.178588];
+imageSize = [578 474];
 imgCol = meshGenerateColored(P,meshColoredFileName,imageSize,BIN_PATH);
 figure;
 imshow(imgCol);

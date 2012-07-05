963dece2ab8345791bb649d8b2d0142c0b6c61e6
brussell123 <bcr@cs.washington.edu>
2012-07-05 15:03:48 -0700

    removed jpeg library and unneeded OBJ files

--- code/LIBS/GenerateLambertian/OBJ.Darwin/generate_lambertian.d
+++
@@ -1,7 +0,0 @@
-OBJ.Darwin/generate_lambertian.o: generate_lambertian.cc \
-  ../trimesh2/include/TriMesh.h ../trimesh2/include/Vec.h \
-  ../trimesh2/include/Color.h ../trimesh2/include/XForm.h \
-  ../trimesh2/include/lineqn.h ../trimesh2/include/GLCamera.h \
-  ../trimesh2/include/XForm.h ../trimesh2/include/timestamp.h \
-  ../trimesh2/include/ICP.h ../trimesh2/include/TriMesh.h \
-  ../trimesh2/include/KDtree.h ../trimesh2/include/GL/glut.h

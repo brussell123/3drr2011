c932fb0c93b388588a043b4a1f0d123379e2395e
brussell123 <bcr@cs.washington.edu>
2012-07-05 15:17:58 -0700

    added test for generate_colored

--- testBinaries.m
+++ testBinaries.m
@@ -8,3 +8,11 @@ meshColoredFileName = './pompeii_large66_sample_0.1_poisson_depth_14_clean_color
 % fastNN:
 x = single(rand(2,10));
 [aa,bb,vv,nn] = fastNN(x,x);
+
+% meshGenerateColored:
+BIN_PATH = './code/LIBS/GenerateLambertian/generate_colored';
+P = [108.792007 276.910461 -723.832397 -171.125000; 405.367004 654.604614 216.727722 -51.611908; 0.972418 -0.089354 -0.215453 -0.178588];
+imgCol = meshGenerateColored(P,meshColoredFileName,imageSize,BIN_PATH);
+figure;
+imshow(imgCol);
+
